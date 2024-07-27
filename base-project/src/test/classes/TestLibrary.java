b9da966c29608a1564535227764628d86ffc3af4
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2024-07-28 02:25:43 +0330

    Added student tests

--- base-project/src/test/classes/TestLibrary.java
+++ base-project/src/test/classes/TestLibrary.java
@@ -100,14 +100,14 @@ public class TestLibrary {
 
     @Test
     public void testSearchBooksByAuthor() {
-        ArrayList<Book> books = new ArrayList<Book>();
+        ArrayList<Book> books = new ArrayList<>();
         books.add(new Book("Book 1", "Author 1", 1));
         books.add(new Book("Book 2", "Author 2", 2));
         books.add(new Book("Book 3", "Author 3", 3));
         books.add(new Book("Book 4", "Author 1", 4));
         books.add(new Book("Book 5", "Author 5", 5));
 
-        ArrayList<Object> keys = new ArrayList<Object>();
+        ArrayList<Object> keys = new ArrayList<>();
         keys.add("Author 1");
         keys.add("Author 2");
 
@@ -118,6 +118,53 @@ public class TestLibrary {
         assertEquals(result.get(0), books.get(0));
         assertEquals(result.get(1), books.get(1));
         assertEquals(result.get(2), books.get(3));
+        assertEquals(result.size(), 3);
+    }
+
+    @Test
+    public void testSearchStudentByID() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Student 1", 1));
+        students.add(new Student("Student 1", 2));
+        students.add(new Student("Student 1", 3));
+        students.add(new Student("Student 1", 4));
+        students.add(new Student("Student 1", 5));
+
+        ArrayList<Object> keys = new ArrayList<>();
+        keys.add("Student 1");
+
+        Library library = new Library();
+        for (Student student : students) library.addStudent(student);
+
+        ArrayList<Student> result = library.searchStudents(SearchByType.NAME, keys);
+        assertEquals(result.get(0), students.get(0));
+        assertEquals(result.get(1), students.get(1));
+        assertEquals(result.get(2), students.get(2));
+        assertEquals(result.get(3), students.get(3));
+        assertEquals(result.get(4), students.get(4));
+        assertEquals(result.size(), 5);
+    }
+
+    @Test
+    public void testSearchStudentByName() {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Student 1", 1));
+        students.add(new Student("Student 2", 2));
+        students.add(new Student("Student 3", 3));
+        students.add(new Student("Student 4", 4));
+        students.add(new Student("Student 5", 4));
+
+        ArrayList<Object> keys = new ArrayList<>();
+        keys.add(3);
+        keys.add(4);
+
+        Library library = new Library();
+        for (Student student : students) library.addStudent(student);
+
+        ArrayList<Student> result = library.searchStudents(SearchByType.ID, keys);
+        assertEquals(result.get(0), students.get(2));
+        assertEquals(result.get(1), students.get(3));
+        assertEquals(result.get(2), students.get(4));
         assertEquals(result.size(), 3);
     }
 }
