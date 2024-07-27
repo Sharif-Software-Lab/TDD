bd396a7773e77d019345b1750b6c5dacdf97f46b
Amirhossein Abedi <amirhoseinabedi80@gmail.com>
2024-07-28 02:10:32 +0330

    Added book search tests

--- base-project/src/test/classes/TestLibrary.java
+++ base-project/src/test/classes/TestLibrary.java
@@ -2,11 +2,13 @@ package test.classes;
 
 import main.classes.Book;
 import main.classes.Library;
+import main.classes.SearchByType;
 import main.classes.Student;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
 
 public class TestLibrary {
     @Test
@@ -47,5 +49,75 @@ public class TestLibrary {
         library.addStudent(student1);
         assertTrue(library.returnBook(book1, student1));
         assertFalse(student1.hasBook(book1));
+    }
+
+    @Test
+    public void testSearchBooksByID() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Book 1", "Author 1", 1));
+        books.add(new Book("Book 2", "Author 2", 2));
+        books.add(new Book("Book 3", "Author 3", 3));
+        books.add(new Book("Book 4", "Author 4", 4));
+        books.add(new Book("Book 5", "Author 5", 5));
+
+        ArrayList<Object> keys = new ArrayList<Object>();
+        keys.add(1);
+        keys.add(3);
+        keys.add(5);
+
+        Library library = new Library();
+        for (Book book : books) library.addBook(book);
+
+        ArrayList<Book> result = library.searchBooks(SearchByType.ID, keys);
+        assertEquals(result.get(0), books.get(0));
+        assertEquals(result.get(1), books.get(2));
+        assertEquals(result.get(2), books.get(4));
+        assertEquals(result.size(), 3);
+    }
+
+    @Test
+    public void testSearchBooksByTitle() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Book 1", "Author 1", 1));
+        books.add(new Book("Book 2", "Author 2", 2));
+        books.add(new Book("Book 1", "Author 3", 3));
+        books.add(new Book("Book 1", "Author 4", 4));
+        books.add(new Book("Book 1", "Author 5", 5));
+
+        ArrayList<Object> keys = new ArrayList<Object>();
+        keys.add("Book 1");
+
+        Library library = new Library();
+        for (Book book : books) library.addBook(book);
+
+        ArrayList<Book> result = library.searchBooks(SearchByType.TITLE, keys);
+        assertEquals(result.get(0), books.get(0));
+        assertEquals(result.get(1), books.get(2));
+        assertEquals(result.get(2), books.get(3));
+        assertEquals(result.get(3), books.get(4));
+        assertEquals(result.size(), 4);
+    }
+
+    @Test
+    public void testSearchBooksByAuthor() {
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Book 1", "Author 1", 1));
+        books.add(new Book("Book 2", "Author 2", 2));
+        books.add(new Book("Book 3", "Author 3", 3));
+        books.add(new Book("Book 4", "Author 1", 4));
+        books.add(new Book("Book 5", "Author 5", 5));
+
+        ArrayList<Object> keys = new ArrayList<Object>();
+        keys.add("Author 1");
+        keys.add("Author 2");
+
+        Library library = new Library();
+        for (Book book : books) library.addBook(book);
+
+        ArrayList<Book> result = library.searchBooks(SearchByType.AUTHOR, keys);
+        assertEquals(result.get(0), books.get(0));
+        assertEquals(result.get(1), books.get(1));
+        assertEquals(result.get(2), books.get(3));
+        assertEquals(result.size(), 3);
     }
 }
